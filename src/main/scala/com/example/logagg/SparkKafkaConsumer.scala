51cdb004312626bbe627fd2ccf2c4cb32fa9830b
Eyouel Kibret <eyouel2016@gmail.com>
2025-11-15 17:46:05 -0500

    add top endpoints count and top error endpoints count

--- src/main/scala/com/example/logagg/SparkKafkaConsumer.scala
+++ src/main/scala/com/example/logagg/SparkKafkaConsumer.scala
@@ -3,6 +3,7 @@ package com.example.logagg
 import org.apache.spark.sql.{DataFrame, SparkSession}
 import org.apache.spark.sql.functions._
 import org.apache.spark.sql.streaming.Trigger
+import org.apache.spark.sql.expressions.Window
 
 object SparkKafkaConsumer extends App {
 
@@ -12,7 +13,6 @@ object SparkKafkaConsumer extends App {
     .config("spark.cassandra.connection.host", "127.0.0.1")
     .config("spark.cassandra.connection.port", "9042")
     .config("spark.sql.extensions", "com.datastax.spark.connector.CassandraSparkExtensions")
-//    .config("spark.sql.streaming.checkpointLocation", "file:///tmp/spark-checkpoint")
     .config("spark.driver.bindAddress", "127.0.0.1")
     .config("spark.ui.port", "4050")
     .getOrCreate()
@@ -40,83 +40,139 @@ object SparkKafkaConsumer extends App {
   )
 
   val logCounts = typeAdded
-    .groupBy(
-      window(col("event_ts"), "10 seconds"),
-      col("type")
-    )
+    .groupBy(window(col("event_ts"), "10 seconds"), col("type"))
     .count()
     .select(
       col("window.start").cast("timestamp").as("window_start"),
-      col("window.end").   cast("timestamp").as("window_end"),
+      col("window.end").cast("timestamp").as("window_end"),
       col("type"),
       col("count").as("cnt")
     )
 
+  val typeCounts10s = logCounts
+    .select(
+      col("window_start"),
+      col("type"),
+      col("cnt")
+    )
 
   val nginxLogs = typeAdded
     .filter(col("type") === "nginx")
     .withColumn("status", regexp_extract(col("log"), """HTTP/1\.1" (\d{3})""", 1).cast("int"))
     .withColumn("is_error", when(col("status") >= 400, 1).otherwise(0))
+    .withColumn("bytes", regexp_extract(col("log"), """ (\d+)$""", 1).cast("long"))
+    .withColumn("endpoint", regexp_extract(col("log"), """GET (/\S*)""", 1))
 
-  val errorRate30s = nginxLogs
-    .groupBy(window(col("event_ts"), "30 seconds"))
+
+
+
+
+  val topEndpointsBase = nginxLogs
+    .groupBy(window(col("event_ts"), "30 seconds"), col("endpoint"))
     .agg(
-      sum("is_error").as("error_logs"),
-      count("*").as("total_logs")
+      count("*").as("hits"),
+      sum("bytes").as("total_bytes"),
+      sum("is_error").as("error_hits")
     )
-    .withColumn("error_rate", col("error_logs").cast("double") / col("total_logs"))
     .select(
       col("window.start").cast("timestamp").as("window_start"),
-      col("error_rate"),
-      col("total_logs"),
-      col("error_logs")
+      col("window.end").cast("timestamp").as("window_end"),
+      col("endpoint"),
+      col("hits"),
+      col("total_bytes"),
+      col("error_hits")
+    )
+
+  val topErrorsBase = nginxLogs
+    .filter(col("status") >= 400)
+    .withColumn(
+      "error_msg",
+      regexp_extract(col("log"), "\"[A-Z]+ ([^ ]+) HTTP", 1)
+    )
+    .groupBy(
+      window(col("event_ts"), "30 seconds"),
+      col("error_msg")
+    )
+    .agg(count("*").as("cnt"))
+    .select(
+      col("window.start").cast("timestamp").as("window_start"),
+      col("window.end").cast("timestamp").as("window_end"),
+      col("error_msg").as("message"),
+      col("cnt")
     )
 
 
-  def writeLogCounts(batchDF: DataFrame, batchId: Long): Unit = {
+  def writeTopEndpoints(batchDF: DataFrame, batchId: Long): Unit = {
     if (!batchDF.isEmpty) {
-      println(s"\n[info] Batch $batchId → log_counts")
-      batchDF.show(truncate = false)
-      batchDF.write
+      val ranked = batchDF
+        .withColumn("rank",
+          row_number().over(Window.partitionBy("window_start").orderBy(col("hits").desc))
+        )
+        .filter(col("rank") <= 10)
+        .select(
+          col("window_start"),
+          col("window_end"),
+          col("rank"),
+          col("endpoint"),
+          col("hits"),
+          col("total_bytes"),
+          col("error_hits")
+        )
+      println(s"\n[info] Batch $batchId → top_endpoints_30s")
+      ranked.orderBy("window_start", "rank").show(50, truncate = false)
+      ranked.write
         .format("org.apache.spark.sql.cassandra")
-        .options(Map(
-          "keyspace" -> "log_ks",
-          "table"    -> "log_counts"
-        ))
+        .options(Map("keyspace" -> "log_ks", "table" -> "top_endpoints_30s"))
         .mode("append")
         .save()
     }
   }
 
-  def writeErrorRate(batchDF: DataFrame, batchId: Long): Unit = {
+  def writeTopErrors(batchDF: DataFrame, batchId: Long): Unit = {
     if (!batchDF.isEmpty) {
-      println(s"\n[info] Batch $batchId → error_rate_30s")
-      batchDF.show(truncate = false)
-      batchDF.write
+
+      val ranked = batchDF
+        .withColumn(
+          "rank",
+          row_number().over(
+            Window.partitionBy("window_start")
+              .orderBy(col("cnt").desc)
+          )
+        )
+        .filter(col("rank") <= 5)
+        .select(
+          col("window_start"),
+          col("window_end"),
+          col("rank"),
+          col("message"),
+          col("cnt")
+        )
+
+      println(s"\n[info] Batch $batchId → top_errors_30s")
+      ranked.orderBy("window_start", "rank").show(50, truncate = false)
+
+      ranked.write
         .format("org.apache.spark.sql.cassandra")
-        .options(Map(
-          "keyspace" -> "log_ks",
-          "table"    -> "error_rate_30s"
-        ))
+        .options(Map("keyspace" -> "log_ks", "table" -> "top_errors_30s"))
         .mode("append")
         .save()
     }
   }
 
-  val q1 = logCounts.writeStream
-    .outputMode("update")
-    .foreachBatch(writeLogCounts _)
-    .trigger(Trigger.ProcessingTime("10 seconds"))
-    .option("checkpointLocation", "file:///tmp/spark-checkpoint/log_counts")
-    .start()
-
-  val q2 = errorRate30s.writeStream
+  val query1 = topEndpointsBase.writeStream
     .outputMode("complete")
-    .foreachBatch(writeErrorRate _)
+    .foreachBatch(writeTopEndpoints _)
     .trigger(Trigger.ProcessingTime("30 seconds"))
-    .option("checkpointLocation", "file:///tmp/spark-checkpoint/error_rate_30s")
+    .option("checkpointLocation", "file:///tmp/spark-checkpoint/top_endpoints_30s")
     .start()
 
-  q1.awaitTermination()
-  q2.awaitTermination()
+  val query2 = topErrorsBase.writeStream
+    .outputMode("complete")
+    .foreachBatch(writeTopErrors _)
+    .trigger(Trigger.ProcessingTime("30 seconds"))
+    .option("checkpointLocation", "file:///tmp/spark-checkpoint/top_errors_30s")
+    .start()
+
+  query1.awaitTermination()
+  query2.awaitTermination()
 }
